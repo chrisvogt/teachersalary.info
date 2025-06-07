d9d7f28c2c0426e577ef3d0558e960ea5c01e12e
Chris Vogt <mail@chrisvogt.me>
2025-06-06 21:45:35 -0700

    feat: update and improve text on site (#28)
    This pull request includes updates to improve the user interface of the
    `Promo` component, refine text content across the application, and
    increment the package version. The most significant changes involve
    simplifying the layout of the `Promo` component, enhancing text
    alignment and padding, and updating textual content for clarity and
    accuracy.
    
    ### UI Updates to `Promo` Component:
    *
    [`src/components/Promo/index.jsx`](diffhunk://#diff-7df2cc002ed20260963b4a7becff3cd86f835cd29a061278af3e98c1594307daL2):
    Removed usage of `react-flexbox-grid` and replaced the grid-based layout
    with simpler `div` elements for better maintainability and readability.
    [[1]](diffhunk://#diff-7df2cc002ed20260963b4a7becff3cd86f835cd29a061278af3e98c1594307daL2)
    [[2]](diffhunk://#diff-7df2cc002ed20260963b4a7becff3cd86f835cd29a061278af3e98c1594307daL17-R19)
    [[3]](diffhunk://#diff-7df2cc002ed20260963b4a7becff3cd86f835cd29a061278af3e98c1594307daL40-L42)
    *
    [`src/styles/Promo.css`](diffhunk://#diff-7a44fcc6b2053aef2992a8965ec948b009140fa514bdb994b6b58c7626c492faR3-R8):
    Adjusted styles for the `Promo` component, including adding centered
    text alignment, reducing padding, and refining margins.
    
    ### Text Content Refinements:
    *
    [`src/content/text.js`](diffhunk://#diff-4e46e79f3f545a72cca0f5004b5121a2dd21c5c318b7a4ceb00d12d73a3e7e5bL7-R17):
    Updated various text strings to improve clarity, accuracy, and alignment
    with the application's purpose, including changes to the `disclaimer`,
    `headline`, `overview`, `source`, and `subhead`.
    
    ### Package Version Update:
    *
    [`package.json`](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519L3-R3):
    Incremented the package version from `0.11.0` to `0.11.1`.

--- src/content/text.js
+++ src/content/text.js
@@ -4,17 +4,17 @@ export const copyright = '© 2017-2025 Chris Vogt'
 
 export const ctaButtonText = 'Visit website'
 
-export const disclaimer = 'Constant dollars based on the Consumer Price Index (CPI), prepared by the Bureau of Labor Statistics, U.S. Department of Labor, adjusted to a school-year basis. The CPI does not account for differences in inflation rates from state to state.'
+export const disclaimer = 'Constant dollars are based on the Consumer Price Index (CPI), prepared by the Bureau of Labor Statistics, U.S. Department of Labor, and adjusted to a school-year basis. The CPI does not reflect differences in inflation rates across individual states.'
 
-export const headline = 'Average teacher salary by state'
+export const headline = 'Average public school teacher salary by state (adjusted for inflation)'
 
 export const noStatesNotice = 'No states selected'
 
-export const overview = 'This page is an experiment by @c1v0 created using JavaScript and teacher salary data from the National Center for Education Statistics (NCES).'
+export const overview = 'This is a data visualization experiment by Chris Vogt using estimated teacher salary data reported by states and published by the National Center for Education Statistics (NCES). It shows how average public school teacher salaries have changed over time across U.S. states, adjusted for inflation using CPI.'
 
-export const source = 'Digest of Education Statistics. (2013, April). Estimated average annual salary of teachers in public elementary and secondary schools by state: Selected years, 1969-70 through 2012-13 [Table]. Retrieved from https://nces.ed.gov/programs/digest/d13/tables/dt13_211.60.asp.'
+export const source = 'Digest of Education Statistics. (2013, April). Estimated average annual salary of teachers in public elementary and secondary schools, by state: Selected years, 1969–70 through 2012–13 [Table]. Retrieved from https://nces.ed.gov/programs/digest/d13/tables/dt13_211.60.asp.'
 
-export const subhead = 'Compare the estimated average annual salary of teachers in public elementary and secondary schools, by U.S. state.'
+export const subhead = 'Explore and compare the estimated average annual salaries of public elementary and secondary school teachers across U.S. states over time.'
 
 export const quote = {
   text: '“But what you learn, as you get older, is that there are a few billion other people in the world all trying to be clever at the same time, and whatever you do with your life will certainly be lost—swallowed up in the ocean—unless you are doing it along with like-minded people who will remember your contributions and carry them forward.”',
